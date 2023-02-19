aa42f819dcfa6aedbc59b1d070b18ffbd1855084
Juego-programacioon <nithson.barragan.03@gmail.com>
2023-02-18 21:28:22 -0500

    primero

---
+++ login.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+
+    setTimeout(() => {
+        this.document.getElementById("pantalla_cargaid").classList.toggle("pantalla_carga_exito")
+
+      },2500);
+    
+})
